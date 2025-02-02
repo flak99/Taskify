af98128345466261be5a0a6f37570c020ef29c6a
inż. Krystian Flak <flak99@o2.pl>
2025-02-02 02:27:17 +0100

    Dodanie cześci z datami
    Sam wyglad bez funkcjonalności

---
+++ Taskify/src/Components/Dates.jsx
@@ -0,0 +1,22 @@
+export function Dates() {
+  return (
+    <>
+      <div className="dates-background">
+        <div className="dates-cont">
+          <div className="date-view" id="date-yesterday">
+            <p>01.02</p>
+            <p>NIE</p>
+          </div>
+          <div className="date-view" id="date-today">
+            <p>01.02</p>
+            <p>NIE</p>
+          </div>
+          <div className="date-view" id="date-tomorrow">
+            <p>01.02</p>
+            <p>NIE</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- Taskify/src/main.jsx
+++ Taskify/src/main.jsx
@@ -1,7 +1,14 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Header } from "./Components/Header";
+import { Dates } from "./Components/Dates";
 
 import "./Styles/Header.css";
+import "./Styles/Dates.css";
 
-createRoot(document.getElementById("root")).render(<Header />);
+createRoot(document.getElementById("root")).render(
+  <>
+    <Header />
+    <Dates />
+  </>
+);
