6a2e20f4a2431cc75bdbdb9cc1711d38d3050387
inż. Krystian Flak <flak99@o2.pl>
2025-02-02 01:55:11 +0100

    Stworzenie Header'a

--- Taskify/src/Components/Header.jsx
+++ Taskify/src/Components/Header.jsx
@@ -1,3 +1,27 @@
+import { IoMenuSharp } from "react-icons/io5";
+
 export function Header() {
-  return console.log("Hello React");
+  return (
+    <>
+      <header>
+        <div className="header-part">
+          <div className="menu-bar">
+            <IoMenuSharp size={50} />
+          </div>
+          <div className="data-cont">
+            <div className="data-label">2 LUT</div>
+          </div>
+        </div>
+        <div className="header-part">
+          <div className="amount-tast-cont font-style">
+            <p>DZISIAJ</p>
+            <div className="amount-task font-style"> 0 zadań</div>
+          </div>
+          <div className="add-task-btn-cont">
+            <button className="add-task-btn">+ Dodaj Zadanie</button>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
--- Taskify/src/main.jsx
+++ Taskify/src/main.jsx
@@ -2,4 +2,6 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Header } from "./Components/Header";
 
+import "./Styles/Header.css";
+
 createRoot(document.getElementById("root")).render(<Header />);
