9489c54704161a7a579d281a5ac1ec4b0a624ac2
inż. Krystian Flak <flak99@o2.pl>
2025-02-02 01:09:26 +0100

    Stworzenie środowkiska pracy w React.js

---
+++ Taskify/src/Components/Header.jsx
@@ -0,0 +1,3 @@
+export function Header() {
+  return console.log("Hello React");
+}
---
+++ Taskify/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Header } from "./Components/Header";
+
+createRoot(document.getElementById("root")).render(<Header />);
