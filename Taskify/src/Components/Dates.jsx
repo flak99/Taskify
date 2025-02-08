d0b9a43fb82bbf0bf8181f49f7173ae7028858a4
inż. Krystian Flak <flak99@o2.pl>
2025-02-09 00:07:05 +0100

    Dodano folder utils
    Bedą pisane tam funckje pomocniczne

--- Taskify/src/Components/Dates.jsx
+++ Taskify/src/Components/Dates.jsx
@@ -5,6 +5,7 @@ export function Dates() {
         <div className="dates-cont">
           <div className="date-view" id="date-yesterday">
             <p>01.02</p>
+
             <p>NIE</p>
           </div>
           <div className="date-view" id="date-today">
---
+++ Taskify/src/utils/getDate.js
@@ -0,0 +1,7 @@
+const daysName = ["NIE", "PO", "WT", "ŚR", "CZW", "PI", "SOB"];
+
+export function GetDateTommorow() {}
+
+export function GetDateToday() {}
+
+export function GetDateYesterday() {}
