cc14c5076a379d2f6a0fa03003b700bfa1cf6ff5
inż. Krystian Flak <flak99@o2.pl>
2025-02-09 00:28:25 +0100

    Dodanie logiki JS do komoponentu z datami
    Zaprogramowano algorymt pobierania i wyseltania nazwy dnia (NIE, PO, Wt.
    ...)

--- Taskify/src/Components/Dates.jsx
+++ Taskify/src/Components/Dates.jsx
@@ -1,20 +1,33 @@
+import {
+  GetDayNameToday,
+  GetDayNameYesterday,
+  GetDayNameTommorow,
+} from "../utils/getDate";
+
 export function Dates() {
   return (
     <>
       <div className="dates-background">
         <div className="dates-cont">
           <div className="date-view" id="date-yesterday">
-            <p>01.02</p>
+            <p>01.01</p>
 
-            <p>NIE</p>
+            <p>
+              <GetDayNameYesterday />
+            </p>
           </div>
           <div className="date-view" id="date-today">
             <p>01.02</p>
-            <p>NIE</p>
+            <p>
+              {" "}
+              <GetDayNameToday />
+            </p>
           </div>
           <div className="date-view" id="date-tomorrow">
             <p>01.02</p>
-            <p>NIE</p>
+            <p>
+              <GetDayNameTommorow />
+            </p>
           </div>
         </div>
       </div>
--- Taskify/src/utils/getDate.js
+++ Taskify/src/utils/getDate.js
@@ -1,7 +1,34 @@
 const daysName = ["NIE", "PO", "WT", "ŚR", "CZW", "PI", "SOB"];
+let date = new Date();
 
-export function GetDateTommorow() {}
+export function GetDayNameYesterday() {
+  let day = date.getDay();
+  let yesterday;
 
-export function GetDateToday() {}
+  if (day === 0) {
+    return (yesterday = daysName[6]);
+  } else {
+    yesterday = daysName[day - 1];
+  }
 
-export function GetDateYesterday() {}
+  return yesterday;
+}
+
+export function GetDayNameToday() {
+  let day = date.getDay();
+  let today = daysName[day];
+
+  return today;
+}
+
+export function GetDayNameTommorow() {
+  let day = date.getDay();
+  let tommorow;
+  if (day === 6) {
+    return (tommorow = daysName[0]);
+  } else {
+    tommorow = daysName[day + 1];
+  }
+
+  return tommorow;
+}
