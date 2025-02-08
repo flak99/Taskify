1a330eb9a4b987698ec6cf0595cc865f10ec564d
inż. Krystian Flak <flak99@o2.pl>
2025-02-08 23:10:18 +0100

    Stworzenie wyglądu (bez logiki JS) kontenera na zadania
    Dodatkowo usunięto border z oliku Dates.css

---
+++ Taskify/src/Components/Tasks.jsx
@@ -0,0 +1,12 @@
+export function Tasks() {
+  return (
+    <>
+      <div className="tasks-area">
+        <div className="task">
+          <div className="task-name">Zrobić pranie</div>
+          <button className="delete-btn">usuń</button>
+        </div>
+      </div>
+    </>
+  );
+}
--- Taskify/src/main.jsx
+++ Taskify/src/main.jsx
@@ -2,13 +2,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Header } from "./Components/Header";
 import { Dates } from "./Components/Dates";
+import { Tasks } from "./Components/Tasks";
 
 import "./Styles/Header.css";
 import "./Styles/Dates.css";
+import "./Styles/Tasks.css";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
     <Dates />
+    <Tasks />
   </>
 );
