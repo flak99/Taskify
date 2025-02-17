185485fcbaf4a8c309065c4615cb1e2f076ab508
inż. Krystian Flak <flak99@o2.pl>
2025-02-18 00:42:45 +0100

    Stworzono modal dodawania zadan oraz logike jego wystelania `

--- Taskify/src/Components/Header.jsx
+++ Taskify/src/Components/Header.jsx
@@ -1,10 +1,31 @@
 /* eslint-disable react/prop-types */
+import { useState } from "react";
 import { IoMenuSharp } from "react-icons/io5";
 
 export function Header({ toggleSideModal }) {
+  const [isAddTaskModalVisible, setIsAddTaskMOdalVisible] = useState(false);
+
+  function ToggleModalVisibility() {
+    setIsAddTaskMOdalVisible((prevSet) => !prevSet);
+  }
+
   return (
     <>
       <header>
+        {isAddTaskModalVisible && (
+          <div className="add-task-modal-cont">
+            <div className="modal-depe">
+              <input
+                type="text"
+                className="task-input"
+                placeholder="Dodaj Zadanie"
+              />
+              <button className="subbmit-btn" onClick={ToggleModalVisibility}>
+                Dodaj Zadanie
+              </button>
+            </div>
+          </div>
+        )}
         <div className="header-part">
           <div className="menu-bar" onClick={toggleSideModal}>
             <IoMenuSharp size={50} />
@@ -19,7 +40,9 @@ export function Header({ toggleSideModal }) {
             <div className="amount-task font-style"> 0 zadań</div>
           </div>
           <div className="add-task-btn-cont">
-            <button className="add-task-btn">+ Dodaj Zadanie</button>
+            <button className="add-task-btn" onClick={ToggleModalVisibility}>
+              + Dodaj Zadanie
+            </button>
           </div>
         </div>
       </header>
